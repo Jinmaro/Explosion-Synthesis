10264e118df1a24dd68424260911e213998f33cf
Jinmaro <59862801+Jinmaro@users.noreply.github.com>
2022-11-25 13:31:16 +0900

    change setting of plot.py

--- src/plot.py
+++ src/plot.py
@@ -33,20 +33,26 @@ def result_plot(path, src, trg):
     #### plot wavs and mels
     plt.rcParams["font.size"] = 18
     ## Src wave
-    ax = fig.add_subplot(2,7,(5,7))
+    ax = fig.add_subplot(2,7,(5,7),xlabel="time [sec]")
+    ax.set(ylim=[-1.0,1.0])
     librosa.display.waveshow(src, sr=hp.sr)
+    plt.xticks( np.arange(0, len(src)/sr, 1))
     ax.set_title('wave of Input', fontsize=15)
     ## Src mel
-    ax = fig.add_subplot(2,7,(1,4))
+    ax = fig.add_subplot(2,7,(1,4),xlabel="time [sec]")
     librosa.display.specshow(mel_src, sr=hp.sr, x_axis='time', y_axis='mel')
+    plt.xticks( np.arange(0, len(src)/sr, 1))
     ax.set_title('Melspectrogram of Input', fontsize=15)
     ## Trg wave
-    ax = fig.add_subplot(2,7,(12,14))
+    ax = fig.add_subplot(2,7,(12,14),xlabel="time [sec]")
+    ax.set(ylim=[-1.0,1.0])
     librosa.display.waveshow(trg, sr=hp.sr)
+    plt.xticks( np.arange(0, len(trg)/sr, 1))
     ax.set_title('wave of Output', fontsize=15)
     ## Trg mel
-    ax = fig.add_subplot(2,7,(8,11))
+    ax = fig.add_subplot(2,7,(8,11),xlabel="time [sec]")
     librosa.display.specshow(mel_trg, sr=hp.sr, x_axis='time', y_axis='mel')
+    plt.xticks( np.arange(0, len(trg)/sr, 1))
     ax.set_title('Melspectrogram of Output', fontsize=15)
 
     plt.rcParams["svg.fonttype"] = "none"
